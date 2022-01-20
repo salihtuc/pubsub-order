0b037aa44180a0fd502c5b2a2c9d5bd984f067c8
Salih Tuc <salihtuc@hotmail.com>
2022-01-20 15:28:59 +0300

    Update test.py

--- test.py
+++ test.py
@@ -53,7 +53,47 @@ def test_random_endpoint():
 
 
 # Test get_orders: Get all orders
-def test_get_orders():
+def test_post_orders():
     response = client.post("/orders/")
     assert response.status_code == 405
 
+
+# Test new_order: Send GET to "New" endpoint
+def test_get_new_order():
+    response = client.get("/orders/new/")
+    assert response.status_code == 405
+
+
+# Test new_order: Send POST to "New" with valid body
+def test_post_order():
+    # TODO add true json here
+    response = client.post("/orders/new", json={})
+    assert response.status_code == 200
+
+    response_json = response.json()
+
+    assert "response" in response_json
+    assert "user" in response_json["response"]
+    assert "order_date" in response_json["response"]
+    assert "foods" in response_json["response"]
+
+
+# Test new_order: Send POST to "New" with invalid body
+def test_post_order_with_wrong_body():
+    response = client.post("/orders/new", json={"foo": "bar"})
+    assert response.status_code == 500  # TODO: Add the response code
+
+
+# Test new_order: Send POST to "New" with empty body
+def test_post_order_with_empty_body():
+    response = client.post("/orders/new")
+    assert response.status_code == 400
+    assert response.json() == {"error": "Cannot get the input Order from the client."}
+
+
+# Test complete_order: Complete order orders
+# TODO: We may need to check the queue for that testing
+def test_post_complete_order():
+    response = client.post("/orders/complete")
+    assert response.status_code == 200
+    assert response.json() == {"success": True}
